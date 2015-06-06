d70b2de81d55e6593a867244ee09311ab4a30703
Jaco Bos <bos.jch@gmail.com>
2015-06-06 22:02:21 +0200

    Update funcptr_statemachine.c

--- funcptr_statemachine.c
+++ funcptr_statemachine.c
@@ -7,19 +7,19 @@ void (*statefunc)() = led_on;
 static int led = 0;
 
 void led_on() {
-	printf("led: %d\n", led = 1);
-	statefunc = led_off;
+    printf("led: %d\n", led = 1);
+    statefunc = led_off;
 }
 
 void led_off() {
-  	printf("led: %d\n", led = 0);
-	statefunc = led_on;
+    printf("led: %d\n", led = 0);
+    statefunc = led_on;
 }
 
 int main() {
-  	while(1) {
-    	(*statefunc)();
-    	sleep(1); 
-  	}
-  	return 1;
+   while(1) {
+       (*statefunc)();
+       sleep(1); 
+  }
+  return 1;
 }
