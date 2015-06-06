54e7e0ebd3f95938353c707e8b8cc1865e1ac83e
Jaco Bos <bos.jch@gmail.com>
2015-06-06 21:57:41 +0200

    Create funcptr_statemachine.c

---
+++ funcptr_statemachine.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void led_on();
+void led_off();
+
+void (*statefunc)() = led_on;
+
+static int led = 0;
+
+void led_on() 
+{
+  	printf("led: %d\n", led = 1);
+	statefunc = led_off;
+}
+
+void led_off() 
+{
+  	printf("led: %d\n", led = 0);
+	statefunc = led_on;
+}
+
+int main() 
+{
+  	while(1) {
+    	(*statefunc)();
+    	sleep(1); 
+  }
+  return 1;
+}
