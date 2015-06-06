01268e80836eab851cef26920054d48f653aae04
Jaco Bos <bos.jch@gmail.com>
2015-06-06 21:59:32 +0200

    reformat

--- funcptr_statemachine.c
+++ funcptr_statemachine.c
@@ -2,28 +2,24 @@
 
 void led_on();
 void led_off();
-
 void (*statefunc)() = led_on;
 
 static int led = 0;
 
-void led_on() 
-{
-  	printf("led: %d\n", led = 1);
+void led_on() {
+	printf("led: %d\n", led = 1);
 	statefunc = led_off;
 }
 
-void led_off() 
-{
+void led_off() {
   	printf("led: %d\n", led = 0);
 	statefunc = led_on;
 }
 
-int main() 
-{
+int main() {
   	while(1) {
     	(*statefunc)();
     	sleep(1); 
-  }
-  return 1;
+  	}
+  	return 1;
 }
