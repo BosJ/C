5418aa94cb5c41f94beec8dfd5f6043d217c4a87
Jaco Bos <bos.jch@gmail.com>
2015-07-24 14:56:56 +0200

    Create problem1.c

---
+++ projecteuler.net/problem1.c
@@ -0,0 +1,21 @@
+/* Solution to https://projecteuler.net/problem=1 
+ * Find the sum of all the multiples of 3 or 5 below 1000. 
+ * Jaco Bos bos.jch@gmail.com */
+
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+    int i, total = 0;
+
+    for(i=0; i<1000; i++) {
+        if( !(i % 3) || !(i % 5) ) {
+          total += i;
+        }
+    } 
+
+    printf("%d\n",total);
+    
+    return 0;
+}
