114454dc0cf4fa0a0c56dfff013a75ebd1cb9c4b
Jaco Bos <bos.jch@gmail.com>
2018-06-14 20:38:11 +0200

    Update pwdmgr.sh

--- pwdmgr.sh
+++ pwdmgr.sh
@@ -1,17 +1,19 @@
 if [ ! -f passwords.txt.enc ]; then
-	# first use
+    # first use
     vi UNSAFE.txt
 else
-	# backup
-	mkdir -p backup
-	timestamp=$(date +%s)
-	cp *.enc backup/passwords.$timestamp.txt.enc 
-	# decrypt
-	openssl enc -aes-256-cbc -d -in passwords.txt.enc -out UNSAFE.txt
-	# edit
-	vi UNSAFE.txt
+    # backup
+    mkdir -p backup
+    timestamp=$(date +%s)
+    cp *.enc backup/passwords.$timestamp.txt.enc 
+    # decrypt
+    openssl enc -aes-256-cbc -d -in passwords.txt.enc -out UNSAFE.txt
+    # edit
+    vi UNSAFE.txt
 fi
 
 # encrypt
 openssl enc -aes-256-cbc -salt -in UNSAFE.txt -out passwords.txt.enc
 rm *.txt
+
+#todo: secure remove and wipe vi buffer
